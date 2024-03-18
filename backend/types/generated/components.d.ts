7a1a06e9f8728be7c357a379026143dcc600b32a
Athira Divakar <athira.di@pitsolutions.com>
2024-03-18 19:55:26 +0530

    Added Home, About, Contact

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface FormContactForm extends Schema.Component {
+  collectionName: 'components_form_contact_forms';
+  info: {
+    displayName: 'Contact Form';
+    icon: 'envelop';
+  };
+  attributes: {
+    Name: Attribute.String & Attribute.Required;
+    Email: Attribute.Email;
+    Message: Attribute.Blocks;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'form.contact-form': FormContactForm;
+    }
+  }
 }
